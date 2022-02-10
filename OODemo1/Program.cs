c2bf075d2b923e90c5a14f37ba6a7e86ef5880cb
Venkat Shiva Reddy <venkat@pratian.com>
2022-02-10 15:10:32 +0530

    oo demo 2

---
+++ OODemo1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo1
+{
+    class Program // A
+    {
+
+        private static Person p = new Person();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am in Program Main");
+            Console.WriteLine( p.GetName());
+            p = new Customer();
+            Console.WriteLine(p.GetName());
+
+        }
+    }
+
+    class Person  // B
+    {
+        public virtual string GetName()
+        {
+            return "I am a person called Ramesh";
+        }
+    }
+
+    class Customer : Person  // B1
+    {
+        public override string GetName()
+        {
+            return "I am a Customer called Ravi";
+        }
+    }
+}
---
+++ OODemo2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doc = "good";
+            HybridParser hp = new HybridParser();
+            hp.SetParser(new ExcellentParser());
+            hp.Parse(doc);
+           
+        }
+    }
+
+    abstract class GenericXMLParser
+    {
+        public abstract void Parse(string doc);
+    }
+    class DOMParser : GenericXMLParser
+    {
+        public override void Parse(string doc)
+        {
+            Console.WriteLine("Parsing using DOM Parser");
+        }
+    }
+    class SAXParser : GenericXMLParser
+    {
+        public override void Parse(string doc)
+        {
+            Console.WriteLine("Parsing using SAX Parser");
+        }
+    }
+    class GoodParser : GenericXMLParser
+    {
+        public override void Parse(string doc)
+        {
+            Console.WriteLine("Parsing using Good Parser");
+        }
+    }
+    class HybridParser : GenericXMLParser
+    {
+        private GenericXMLParser parser;
+
+        public void SetParser(GenericXMLParser p)
+        {
+            parser = p;
+        }
+        public override void Parse(string doc)
+        {
+            parser.Parse(doc);
+        }
+    }
+    class ExcellentParser : GenericXMLParser
+    {
+        public override void Parse(string doc)
+        {
+            Console.WriteLine("Using Excellent Parser");
+        }
+    }
+}
