d21db274081d0736b145112a1b920dc0db01113e
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-02-17 16:44:17 +0530

    Module 2

---
+++ AIDataLoader/BookDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDataLoader
+{
+    public class BookDetails
+    {
+        public List<Book> Books { get; set; }
+    }
+}
