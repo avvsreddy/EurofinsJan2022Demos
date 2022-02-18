0410cb79f1dac18a8e687f43900a2aabba895407
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-02-18 14:27:31 +0530

    module 4

---
+++ AIRecommendationClientApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using AIDataLoader;
+using AIRatingsAggrigator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIRecommendationClientApp
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            AIRecommendationEngine.AIRecommendationApp app = new AIRecommendationEngine.AIRecommendationApp();
+            Preference preference = new Preference { ISBN = 1234567, Age = 25, State = "LA" };
+            List<Book> recommendedBooks = app.Recommend(preference, 10);
+
+            foreach (Book book in recommendedBooks)
+            {
+                Console.WriteLine(book.BookTitle);
+            }
+
+        }
+    }
+}
---
+++ AIRecommendationEngine/AIRecommendationApp.cs
@@ -0,0 +1,23 @@
+﻿using AIDataLoader;
+using AIRatingsAggrigator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIRecommendationEngine
+{
+    public class AIRecommendationApp
+    {
+        public List<Book> Recommend(Preference preference, int limit)
+        {
+            List<Book> recommendedBooksList = new List<Book>();
+            // get the data from data loader
+            // get the aggrigated data from aggrigator
+            // get the coffiecient factor from perason engine
+            // get the recommended books and return
+            return recommendedBooksList;
+        }
+    }
+}
