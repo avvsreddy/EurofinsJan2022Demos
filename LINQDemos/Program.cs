2778dd9354bfda5133a40321b94d8f762a737be8
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-02-22 11:24:33 +0530

    linq demo1

---
+++ LINQDemos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Dept { get; set; }
+
+        public static List<Emp> GetEmps()
+        {
+            List<Emp> emps = new List<Emp> 
+            {
+                new Emp{Id=111, Name="Ramesh", Salary=67000, Dept="HR"},
+                new Emp{Id=345, Name="Girish", Salary=57000, Dept="HR"},
+                new Emp{Id=222, Name="Suresh", Salary=27000, Dept="IT"},
+                new Emp{Id=112, Name="Mahesh", Salary=57000, Dept="HR"},
+                new Emp{Id=211, Name="Kamesh", Salary=67000, Dept="IT"},
+                new Emp{Id=456, Name="Lokesh", Salary=67000, Dept="IT"},
+                new Emp{Id=234, Name="Ravi", Salary=67000, Dept="ACC"},
+                new Emp{Id=678, Name="Suri", Salary=50000, Dept="ACC"},
+                new Emp{Id=178, Name="Sita", Salary=25000, Dept="HR"},
+                new Emp{Id=367, Name="Kamala", Salary=64000, Dept="SW"},
+                new Emp{Id=784, Name="Shreya", Salary=62000, Dept="HR"},
+                new Emp{Id=231, Name="Anitha", Salary=67000, Dept="SW"},
+            };
+            return emps;
+        }
+    }
+}
---
+++ LanguageEnhancementDemo/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageEnhancementDemo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int sum = 0;
+            foreach (var n in numbers)
+            {
+                if( n % 2 == 0)
+                sum += n;
+            }
+
+            //Func<int, bool> filter = new Func<int, bool>(IsEven);
+            //var s = numbers.Where(IsEven).Sum();
+
+            var s = numbers.Where(delegate (int n)
+                {
+                    return n % 2 == 0;
+                }).Sum();
+
+             s = numbers.Where((int n) =>
+            {
+                return n % 2 == 0;
+            }).Sum();
+
+
+            s = numbers.Where((int n) =>
+            {
+                return n % 2 == 0;
+            }).Sum();
+
+
+            s = numbers.Where((int n) =>
+           
+                 n % 2 == 0
+            ).Sum();
+
+            s = numbers.Where(n => n % 2 == 0).Sum();
+
+            // Lambda - Light weight syntax for anymous delegates
+            // Lambda Statements
+            // Lambda Expressions
+            // if(a > 10) return a else return 0 // statemetn
+            // a+b+10-40  // expression
+
+
+            var ss = (from n in numbers
+                      where n % 2 == 0
+                      select n).Sum();
+
+            // SQL => select sum(number) where number mod 2 = 0 from numbers;
+
+            Console.WriteLine(sum);
+
+
+            int? x = null;
+            int? xx = 10;
+            Nullable<int> sal = null;
+            int? salary = null;
+            double? d = null;
+            bool? b = null;
+            string str = null;
+
+            // 1. Local Veriable Type Inference
+            var a =10;
+            //a = "10";
+            var code = "abc";
+            var rate = 56.78;
+            var dict = new Dictionary<string, int>();
+            //var n = int.Parse(Console.ReadLine());
+
+            // 2. object initialization syntax
+            var i1 = new Item();
+            i1.Id = 111;
+            i1.Name = "Pen";
+            i1.Cost = 150;
+            Console.WriteLine(i1.Cost);
+
+            // Object Initialization Syntax
+            //Item i2 = new Item(222,"Book", 250);
+            __anoymous34234234234 i2 = new __anoymous34234234234 { Id= 222, Name ="Book", Cost=250 };
+            
+            var i3 = new  { Id=333 };
+            //i3.Id = 444;
+            var i4 = new  { Name="Pencil" };
+
+            var i5 = new  { Id=555, Name="Note Book" };
+
+            // 3: Anonymous Types
+
+            // 4: Extension Methods
+
+            string data = "some data";
+            data = data.ToUpper();
+            data = data.ToLower();
+            data = data.Substring(1, 2);
+            data = data.Encrypt("MD5");
+            data = DataUtil.Encrypt(data,"SHA256");
+
+
+        }
+
+        //public static bool IsEven(int n)
+        //{
+        //    return n % 2 == 0;
+        //}
+    }
+
+    static class DataUtil
+    {
+        public static string Encrypt(this string data, string type)
+        {
+            return "#$%#$%#FEGDFGDFG%#$^#$^#%^#TRDGERGET#$%#$%";
+        }
+    }
+
+    class __anoymous34234234234
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+
+
+    class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
