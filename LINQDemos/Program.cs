a34cfb83912f452687d4b2f97cd6a8c21d5ff01f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-02-22 12:33:18 +0530

    linq ex

--- LINQDemos/Program.cs
+++ LINQDemos/Program.cs
@@ -10,8 +10,65 @@ namespace LINQDemos
     {
         static void Main(string[] args)
         {
+            //LINQ To Objects
+
+            // 1. Get all employee names and display
+            var r1 = from e in Emp.GetEmps()
+                     select e.Name;
+            var r11 = Emp.GetEmps().Select(e => e.Name);
+
+            foreach (var item in r1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 2. Get all employee names and salaries then display
+            var r2 = from e2 in Emp.GetEmps()
+                     select new  {e2.Name,e2.Salary };
+
+            // 3. Get all emp names from HR dept
+            var r3 = from e3 in Emp.GetEmps()
+                     where e3.Dept == "HR"
+                     select e3.Name;
+            // 4. Get Total Salary of all emps
+            var r4 = Emp.GetEmps().Sum(e => e.Salary);
+
+            var r44 = (from e4 in Emp.GetEmps()
+                       select e4.Salary).Sum();
+
+            // 5. Get Total Salary of emps working in IT dept
+            var r5 = (from e5 in Emp.GetEmps()
+                      where e5.Dept == "IT"
+                      select e5.Salary).Sum();
+            var r55 = Emp.GetEmps().Where(e => e.Dept == "IT").Sum(e => e.Salary);
+            // 6. Get Total count of all emps working in SW dept
+            var r6 = (from e6 in Emp.GetEmps()
+                      where e6.Dept.Equals("SW") select e6).Count();
+
+            // 7. Get the Avg salary of all emp
+            var r7 = Emp.GetEmps().Average(e => e.Salary);
+
+            // 8. Get the name of the emp who is getting highest salary
+            var r8 = (from e8 in Emp.GetEmps()
+                     where e8.Salary == Emp.GetEmps().Max(e => e.Salary)
+                     select e8.Name).FirstOrDefault();
+
+            // 9. Get the name of the emp who is getting lowest salary
+            var r9 = Emp.GetEmps().Where(e9 => e9.Salary == Emp.GetEmps().Min(e => e.Salary)).Select(e => e.Name);
+
+            // 10. Get all employees name and salary who is getting salary more than 75k
+            var r10 = from e10 in Emp.GetEmps()
+                      where e10.Salary >= 75000
+                      select new { e10.Name, e10.Salary };
+
+
         }
     }
+    //class EmpNameSal
+    //{
+    //    public string Name { get; set; }
+    //    public int Salary { get; set; }
+    //}
     class Emp
     {
         public int Id { get; set; }
